35341488af69864dfc5635a260f4f5676671dcf8
Ansu Sharma <ansusharma@Ansus-MacBook-Air.local>
2026-01-26 20:44:29 +0530

    api url update

--- mobile/src/constants/api.js
+++ mobile/src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://192.168.29.85:5001/api';
+export const API_BASE_URL = 'https://velox-app.onrender.com/api';
 
 // For local simulation, you can also use:
 // export const API_BASE_URL = 'http://127.0.0.1:5001/api';
